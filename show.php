5fc908feca856d77827e75d4c80dba74f53b1439
Harrison Wynn <harrisonwynn42@gmail.com>
2019-04-25 09:43:08 -0500

    fixed bug that stopped page if pokemon had no second ability

--- show.php
+++ show.php
@@ -18,7 +18,7 @@
 		</header>
         <div class='centercolumn'>
 		    <div class='half' style='background-color: #ffbbb1;'>
-				<div style='background-color: #ffeca9;'>
+				
 						<?php
 							include 'secret.php';
 							$mysqli = new mysqli($host, $user, $password, $database);
@@ -27,48 +27,95 @@
 								printf("Connect failed: %s\n", $mysqli->connect_error);
 								exit();
 							}
-							
-							if (isset($_GET['pokeid'])) {
-								echo $_GET['pokeid'];
-							} else { echo "pokemon id not found.";}
-
-							$pokeid = 405;
-							$query1 = "SELECT poke_name, poke_type1, poke_type2, abil_name1, abil_name2, abil1.abil_text AS abil_text1, abil2.abil_text AS abil_text2, location, pokedexText FROM `pokemon`,(SELECT abil_text FROM `abilities`, `pokemon` WHERE national_id=" . $pokeid . " AND abil_name1=abil_name)abil1, 
-(SELECT abil_text FROM `abilities`, `pokemon` WHERE national_id=" . $pokeid . " AND abil_name2=abil_name)abil2 WHERE national_id =" . $pokeid;
-							if ($result1 = $mysqli->query($query1)) {
-								/* fetch associative array */
-								if ($row = $result1->fetch_assoc()) {
-									printf ("<h1 style='margin: 0;'>");
-									printf ($row["poke_name"]);
-									echo "</h1></div><div style='background-color: #c7e8ac;'><img src='diamond-pearl/405.png' width='160' height='160'></div><div style='background-color: #c1e4f7; padding-top: 10px; padding-bottom: 10px;'><div class='infobox'><div class='infohead'>Types</div>";
-									printf ($row["poke_type1"]);
-									if (!empty($row["poke_type2"])){printf("        " . $row["poke_type2"]);}
-									echo "</div><div class='infobox'><div class='infohead'>Pokedex Entry</div><div class='infotext'>";
-									printf ($row["pokedexText"]);
-									echo "</div></div>";
-									echo "<div class='infobox'><div class='infohead'>Locations</div><div class='infotext'>" . $row["location"] . "</div></div>";
-									
-									echo "<div class='infobox'><div class='infohead'>Abilities</div><div class='infotext'>";
-									echo "<span>" . $row["abil_name1"] . "</span>: ";
-									echo $row["abil_text1"] . "</div>";
-									if (!empty($row["abil_name2"])){
-										printf("<div class='infotext'><span>" . $row["abil_name2"] . "</span>: " . $row["abil_text2"] . "</div>");}
-									
-									
-									
-									
+							if (isset($_GET['pokeid'])) 
+							{
+								$pokeid = 405;
+								$c_abil2 = FALSE;//c_ is for conditional. This stores if the pokemon has a second ability
+								
+								$query1 = "SELECT abil_text FROM `abilities`, `pokemon` WHERE national_id=" . $_GET['pokeid'] . " AND abil_name2=abil_name;";
+								$query2 = "SELECT poke_name, poke_type1, poke_type2, abil_name1, abil_name2, abil1.abil_text AS abil_text1, abil2.abil_text AS abil_text2, location, pokedexText 
+								FROM `pokemon`,
+								(SELECT abil_text FROM `abilities`, `pokemon` WHERE national_id=" . $_GET['pokeid'] . " AND abil_name1=abil_name)abil1,
+								(SELECT abil_text FROM `abilities`, `pokemon` WHERE national_id=" . $_GET['pokeid'] . " AND abil_name2=abil_name)abil2 
+								WHERE national_id =" . $_GET['pokeid'];
+								$query3 = "SELECT poke_name, poke_type1, poke_type2, abil_name1, abil1.abil_text AS abil_text1, location, pokedexText 
+								FROM `pokemon`,
+								(SELECT abil_text FROM `abilities`, `pokemon` WHERE national_id=" . $_GET['pokeid'] . " AND abil_name1=abil_name)abil1
+								WHERE national_id =" . $_GET['pokeid'];
+								if($result1 = $mysqli->query($query1))
+								{
+									if ($trow = $result1->fetch_assoc()) 
+									{$c_abil2 = TRUE;}
+									else
+									{echo "has no second ability";}
+									/* free result set */
+									$result1->free();
 								}
-								/* free result set */
-								$result1->free();
-							}
-							/* close connection */
-							$mysqli->close();
+								else
+								{ echo "We don't know if it has a 2nd ability or not";}
+								
+								echo "2nd abil: " . $c_abil2;
+								if($c_abil2)
+								{	//if pokemon has second ability, use the query that includes abil2
+									if ($result2 = $mysqli->query($query2)) 
+									{
+										/* fetch associative array */
+										if ($row = $result2->fetch_assoc()) {
+											printf ("<div style='background-color: #ffeca9;'><h1 style='margin: 0;'>");
+											printf ($row["poke_name"]);
+											echo "</h1></div><div style='background-color: #c7e8ac;'><img src='diamond-pearl/" . $_GET['pokeid'] . ".png' width='160' height='160'></div><div style='background-color: #c1e4f7; padding-top: 10px; padding-bottom: 10px;'><div class='infobox'><div class='infohead'>Types</div>";
+											printf ($row["poke_type1"]);
+											if (!empty($row["poke_type2"])){printf("        " . $row["poke_type2"]);}
+											echo "</div><div class='infobox'><div class='infohead'>Pokedex Entry</div><div class='infotext'>";
+											printf ($row["pokedexText"]);
+											echo "</div></div>";
+											echo "<div class='infobox'><div class='infohead'>Locations</div><div class='infotext'>" . $row["location"] . "</div></div>";
+											
+											echo "<div class='infobox'><div class='infohead'>Abilities</div><div class='infotext'>";
+											echo "<span>" . $row["abil_name1"] . "</span>: ";
+											echo $row["abil_text1"] . "</div>";
+											printf("<div class='infotext'><span>" . $row["abil_name2"] . "</span>: " . $row["abil_text2"] . "</div>");
+											echo "</div></div>";
+										}
+										else {echo "no row returned";}
+										/* free result set */
+										$result2->free();
+									}
+									else { echo "bad result2?";}
+								}
+								else
+								{	//if pokemon has no second ability, don't check for abil2
+									if ($result3 = $mysqli->query($query3)) 
+									{
+										/* fetch associative array */
+										if ($row = $result3->fetch_assoc()) {
+											printf ("<div style='background-color: #ffeca9;'><h1 style='margin: 0;'>");
+											printf ($row["poke_name"]);
+											echo "</h1></div><div style='background-color: #c7e8ac;'><img src='diamond-pearl/" . $_GET['pokeid'] . ".png' width='160' height='160'></div><div style='background-color: #c1e4f7; padding-top: 10px; padding-bottom: 10px;'><div class='infobox'><div class='infohead'>Types</div>";
+											printf ($row["poke_type1"]);
+											if (!empty($row["poke_type2"])){printf("        " . $row["poke_type2"]);}
+											echo "</div><div class='infobox'><div class='infohead'>Pokedex Entry</div><div class='infotext'>";
+											printf ($row["pokedexText"]);
+											echo "</div></div>";
+											echo "<div class='infobox'><div class='infohead'>Locations</div><div class='infotext'>" . $row["location"] . "</div></div>";
+											
+											echo "<div class='infobox'><div class='infohead'>Abilities</div><div class='infotext'>";
+											echo "<span>" . $row["abil_name1"] . "</span>: ";
+											echo $row["abil_text1"] . "</div>";
+											echo "</div></div>";
+										}
+										else {echo "no row returned";}
+										/* free result set */
+										$result3->free();
+									}
+									else { echo "bad result3?";}						
+								}
+								
+								
+								/* close connection */
+								$mysqli->close();	
+							} else { echo "pokemon id not found.";}
 						?>
-					</div>
-				</div>
-
-				
-				
 			</div>
 		    <div class='half' style='background-color: #3aa6dd;'>
 				<div id='table_shell'>
@@ -83,99 +130,41 @@
 					</table>
 					<table id='scroll_table'>
 						<tbody>
-							<tr>
-								<td>Tackle</td><td>35</td><td>Level:—</td>
-							</tr>
-							<tr>
-								<td>Leer</td><td>0</td><td>Level:—</td>
-							</tr>
-							<tr>
-								<td>Charge</td><td>0</td><td>Level:—</td>
-							</tr>
-							<tr>
-								<td>Leer</td><td>0</td><td>Level:5</td>
-							</tr>
-							<tr>
-								<td>Charge</td><td>0</td><td>Level:9</td>
-							</tr>
-							<tr>
-								<td>Bite</td><td>60</td><td>Level:60</td>
-							</tr>
-							<tr>
-								<td>Spark</td><td>65</td><td>Level:65</td>
-							</tr>
-							<tr>
-								<td>Roar</td><td>0</td><td>Level:0</td>
-							</tr>
-							<tr>
-								<td>Swagger</td><td>0</td><td>Level:28</td>
-							</tr>
-							<tr>
-								<td>Crunch</td><td>80</td><td>Level:80</td>
-							</tr>
-							<tr>
-								<td>Thunder Fang</td><td>65</td><td>Level:42</td>
-							</tr>
-							<tr>
-								<td>Scary Face</td><td>0</td><td>Level:49</td>
-							</tr>
-							<tr>
-								<td>Discharge</td><td>80</td><td>Level:56</td>
-							</tr>
-							<tr>
-								<td>Roar</td><td>0</td><td>TM05</td>
-							</tr>
-							<tr>
-								<td>Ice Fang</td><td>65</td><td>Egg Move</td>
-							</tr>
-							<tr>
-								<td>Fire Fang</td><td>65</td><td>Egg Move</td>
-							</tr>
-							<tr>
-								<td>Thunder Fang</td><td>65</td><td>Egg Move</td>
-							</tr>
-							<tr>
-								<td>Quick Attack</td><td>35</td><td>Egg Move</td>
-							</tr>
-							<tr>
-								<td>Howl</td><td>35</td><td>Egg Move</td>
-							</tr>
-							<tr>
-								<td>Take Down</td><td>90</td><td>Egg Move</td>
-							</tr>
+							<?php
+								include 'secret.php';
+								$mysqli = new mysqli($host, $user, $password, $database);
+								/* check connection */
+								if ($mysqli->connect_errno) {
+									printf("Connect failed: %s\n", $mysqli->connect_error);
+									exit();
+								}
+								if (isset($_GET['pokeid'])) 
+								{
+									$pokeid = 405;
+									$query = "SELECT poke_name, learn.move_name AS movenm, power, learn_method, move_text FROM `pokemon`, `learn`, `moves` WHERE learn.national_id=pokemon.national_id AND learn.move_name=moves.move_name AND pokemon.national_id=" . $_GET['pokeid'];
+									if ($result = $mysqli->query($query)) {
+										/* fetch associative array */
+										while($row = $result->fetch_assoc()) {
+											printf ("<tr><td>%s</td><td>%s</td><td>%s</td></tr>", $row["movenm"], $row["power"], $row["learn_method"]);
+										}
+										
+										/*while ($row = $result->fetch_assoc()) {
+											printf ("%s (%s)\n", $row["Name"], $row["CountryCode"]);
+										}*/
+
+										/* free result set */
+										$result->free();
+									}
+									/* close connection */
+									$mysqli->close();
+								} else { echo "pokemon id not found.";}
+							?>					
 						</tbody>
 					</table>
 				</div>
 				
 			</div>
 		</div>
-		<?php
-			include 'secret.php';
-			$mysqli = new mysqli($host, $user, $password, $database);
-			/* check connection */
-			if ($mysqli->connect_errno) {
-				printf("Connect failed: %s\n", $mysqli->connect_error);
-				exit();
-			}
 
-			$pokeid = 405;
-			
-			$query = "SELECT poke_name, poke_type1, poke_type2, abil_name1, abil_name2, location, pokedexText FROM  `pokemon` WHERE national_id =405";
-			if ($result = $mysqli->query($query)) {
-				/* fetch associative array */
-				if ($row = $result->fetch_assoc()) {
-					printf ("pokemon name: %s   type1: %s	type2: %s abil_name1:	%s  abil_name2: %s   location: %s	pokedex:  %s", $row["poke_name"], $row["poke_type1"], $row["poke_type2"], $row["abil_name1"], $row["abil_name2"], $row["location"], $row["pokedexText"]);
-				}
-				
-				/*while ($row = $result->fetch_assoc()) {
-					printf ("%s (%s)\n", $row["Name"], $row["CountryCode"]);
-				}*/
-
-				/* free result set */
-				$result->free();
-			}
-			/* close connection */
-			$mysqli->close();
-		?>
     </body>
 </html>
