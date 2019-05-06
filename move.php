665e577c34240c489d335a9cc63ae6febc66b96d
Harrison Wynn <harrisonwynn42@gmail.com>
2019-05-06 11:36:04 -0500

    Move page complete

--- move.php
+++ move.php
@@ -19,27 +19,58 @@
 		</header>
 		<table>
 			<tbody>
-				<tr>
-					<th>Name</th>
-					<td>Absorb</td>
-				</tr>
-				<tr>
-					<th>Type</th>
-					<td>grass</td>
-				</tr>
-				<tr>
-					<th>Category</th>
-					<td>special</td>
-				</tr>
-				<tr>
-					<th>Power</th>
-					<td>20</td>
-				</tr>
-				<tr>
-					<th>Description</th>
-					<td>A nutrient-draining attack. The user's HP is restored by half the damage taken by the target.</td>
-				</tr>
+				<?php
+					include 'secret.php';
+					$mysqli = new mysqli($host, $user, $password, $database);
+					/* check connection */
+					if ($mysqli->connect_errno) {
+						printf("Connect failed: %s\n", $mysqli->connect_error);
+						exit();
+					}
+					if (isset($_GET['movename']))
+					{
+
+						$query1 = "SELECT * FROM `moves` WHERE move_name='". $_GET['movename']."'";
+						if($result1 = $mysqli->query($query1))
+						{
+							if ($row = $result1->fetch_assoc())
+							{
+								echo"<tr>";
+								echo"<th>Name</th>";
+								echo"<td>".$row["move_name"]."</td>";
+								echo"</tr>";
+								echo"<tr>";
+								echo"<th>Type</th>";
+								echo"<td>".$row["move_type"]."</td>";
+								echo"</tr>";
+								echo"<tr>";
+								echo"<th>Category</th>";
+								echo"<td>".$row["category"]."</td>";
+								echo"</tr>";
+								echo"<tr>";
+								echo"<th>Power</th>";
+								echo"<td>".$row["power"]."</td>";
+								echo"</tr>";
+								echo"<tr>";
+								echo"<th>Description</th>";
+								echo"<td>".$row["move_text"]."</td>";
+								echo"</tr>";
+							}
+							/* free result set */
+							$result1->free();
+						}
+						else
+						{ 
+							echo "query error. Nothing found.";
+							exit();
+						}
+					} else { echo "No move name found.";}
+				?>
 			</tbody>
 		</table>
+		
+		
+		
+		
     </body>
 </html>
